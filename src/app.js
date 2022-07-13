4d5cbadd5614788f6061a3b033d3d29e289807c2
NikiWolf93 <105113354+NikiWolf93@users.noreply.github.com>
2022-07-13 18:17:02 +0300

    displayForecast created in JS

--- src/app.js
+++ src/app.js
@@ -23,6 +23,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  
+    <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="36"/>
+          <div class="weather-forecast-temperature"> 
+              <span class="weather-forecast-temperature-max"> 18 </span>
+               <span class="weather-forecast-temperature-min"> 12 </span>
+          </div>
+      </div>      
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -80,8 +105,6 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
-search("Turku");
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -90,3 +113,6 @@ farenheitLink.addEventListener("click", displayFarenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Turku");
+displayForecast();
