fe41ea9ae27c6c3d09b7cfcf34a74ea92eec4af4
NikiWolf93 <105113354+NikiWolf93@users.noreply.github.com>
2022-07-18 11:43:00 +0300

    final design has been added, small tings has been corrected.

--- src/app.js
+++ src/app.js
@@ -41,7 +41,6 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-  
     <div class="col-2">
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img src="https://openweathermap.org/img/wn/${
